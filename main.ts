0b3e711fc6fb140521340b89be49e43c51c35f17
iamalanyang <iamalanyang@gmail.com>
2022-02-05 10:29:36 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,80 @@
+music.playMelody("C D E F G A B C5 ", 150)
+basic.forever(function () {
+    images.createBigImage(`
+        . . # . . . . . . .
+        # # # # # . . . . .
+        # . # . # . . . . .
+        # # # # # . . . . .
+        . . # . . . . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . # . . . . . .
+        . # # # # # . . . .
+        . # . # . # . . . .
+        . # # # # # . . . .
+        . . . # . . . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . # . . . . .
+        . . # # # # # . . .
+        . . # . # . # . . .
+        . . # # # # # . . .
+        . . . . # . . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . . # . . . .
+        . . . # # # # # . .
+        . . . # . # . # . .
+        . . . # # # # # . .
+        . . . . . # . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . . . # . . .
+        . . . . # # # # # .
+        . . . . # . # . # .
+        . . . . # # # # # .
+        . . . . . . # . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . . . . # . .
+        . . . . . # # # # #
+        . . . . . # . # . #
+        . . . . . # # # # #
+        . . . . . . . # . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . . . # . . .
+        . . . . # # # # # .
+        . . . . # . # . # .
+        . . . . # # # # # .
+        . . . . . . # . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . . # . . . .
+        . . . # # # # # . .
+        . . . # . # . # . .
+        . . . # # # # # . .
+        . . . . . # . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . . # . . . . .
+        . . # # # # # . . .
+        . . # . # . # . . .
+        . . # # # # # . . .
+        . . . . # . . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . . # . . . . . .
+        . # # # # # . . . .
+        . # . # . # . . . .
+        . # # # # # . . . .
+        . . . # . . . . . .
+        `).showImage(0, 200)
+    images.createBigImage(`
+        . . # . . . . . . .
+        # # # # # . . . . .
+        # . # . # . . . . .
+        # # # # # . . . . .
+        . . # . . . . . . .
+        `).showImage(0, 200)
+})
